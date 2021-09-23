d832682c8a8d48526f1287803a412ed7cb7c9b24
gülşah <33907502+gulsahsevinel@users.noreply.github.com>
2021-09-23 19:49:11 +0300

    initial commit <3

---
+++ app/src/main/java/com/gulsah/kab/DetailFragment.kt
@@ -0,0 +1,23 @@
+package com.gulsah.kab
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.gulsah.kab.databinding.FragmentDetailBinding
+
+class DetailFragment : Fragment() {
+
+    private lateinit var binding: FragmentDetailBinding
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_detail, container, false)
+        return binding.root
+    }
+
+}
