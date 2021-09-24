4b877170e99773efc0d44da911f168f19da68702
gülşah <33907502+gulsahsevinel@users.noreply.github.com>
2021-09-24 18:32:10 +0300

    icons,colors, toolbar, searchbar and carouselView

--- app/src/main/java/com/gulsah/kab/view/HomePageFragment.kt
+++ app/src/main/java/com/gulsah/kab/view/HomePageFragment.kt
@@ -8,8 +8,11 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import com.gulsah.kab.R
 import com.gulsah.kab.databinding.FragmentHomePageBinding
+import com.synnapps.carouselview.ImageListener
 
 class HomePageFragment : Fragment() {
+
+    var imageArray: ArrayList<Int> = ArrayList()
 
     private lateinit var binding: FragmentHomePageBinding
     override fun onCreateView(
@@ -18,6 +21,20 @@ class HomePageFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home_page, container, false)
+
+        //for carouselView
+        imageArray.add(R.drawable.banner_1)
+        imageArray.add(R.drawable.banner_2)
+        imageArray.add(R.drawable.banner_3)
+
+        binding.carouselView.pageCount = imageArray.size
+        binding.carouselView.setImageListener(imageListener)
+
         return binding.root
     }
+
+    var imageListener = ImageListener { position, imageView ->
+        imageView!!.setImageResource(imageArray[position])
+    }
+
 }
